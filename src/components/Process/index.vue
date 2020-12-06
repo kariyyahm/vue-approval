07929c6b98742886ab28fe69339ba198b07d91e5
kKari <mr_hm@outlook.com>
2020-12-06 21:34:14 +0800

    流程设计

--- src/components/Process/index.vue
+++ src/components/Process/index.vue
@@ -1,9 +1,11 @@
 <!-- 流程设置 -->
 <template>
   <div class="process">
-    <SponsorNode/>
-    <div class="process-over">
-      流程结束
+    <div class="process-tree">
+      <SponsorNode/>
+      <div class="process-over">
+        流程结束
+      </div>
     </div>
     <NodeSettings ref="NodeSettings"/>
   </div>
@@ -31,14 +33,38 @@ export default {
 </script>
 
 <style lang="less" scoped>
+@import '../../index.less';
+
 .process {
   background-color: rgb(238, 241, 243);
   width: 100vw;
   min-height: calc(~"100vh - 45px");
   padding: 10px 0;
+  text-align: center;
 
-  .process-over {
 
+  .process-tree {
+    margin: 60px;
+    display: inline-block;
+
+    .process-over {
+      position: relative;
+      padding-top: 5px;
+      text-align: center;
+      color: rgba(25, 31, 37, 0.4);
+
+      &::before {
+        content: '';
+        position: absolute;
+        left: 50%;
+        top: 0;
+        transform: translate(-50%, -50%);
+        width: 10px;
+        height: 10px;
+        border-radius: 50%;
+        background-color: @connect-line;
+      }
+    }
   }
 }
 </style>
