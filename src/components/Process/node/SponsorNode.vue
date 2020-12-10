14d7f6951cfdaca2529b8b2b6f91425e4d40a8ac
kKari <mr_hm@outlook.com>
2020-12-10 22:24:16 +0800

    流程设计

--- src/components/Process/node/SponsorNode.vue
+++ src/components/Process/node/SponsorNode.vue
@@ -1,15 +1,37 @@
 <!-- 发起人节点 -->
 <template>
-  <div class="sponsor-node">
-    <AddButton/>
+  <div class="node">
+    <div class="sponsor node-wrapper">
+      <div class="node-box">
+        <div class="sponsor node-box-title">
+          <span style="display: flex; align-items: center;">
+            <span class="title-text"> currentNode.name </span>
+          </span>
+          <a-icon type="close" class="icon-close" @click="delNode"/>
+        </div>
+        <div class="node-box-content" @click="editNode">
+          <p class="node-box-content-persons">summary</p>
+        </div>
+      </div>
+      <AddButton class="node-addBtn"/>
+    </div>
+<!--    <div v-if="currentNode.childNode">-->
+<!--      <component-->
+<!--          :is="getNodeComponent(currentNode.childNode)"-->
+<!--          :currentNode="currentNode.childNode"-->
+<!--          :parentNode="currentNode"-->
+<!--          v-on="$listeners"-->
+<!--      />-->
+<!--    </div>-->
   </div>
 </template>
 
 <script>
 import AddButton from "./AddButton";
+import BaseNode from "@/components/Process/node/BaseNode";
 
 export default {
-  mixin: [],
+  mixins: [BaseNode],
   components: {
     AddButton
   },
@@ -20,12 +42,7 @@ export default {
   computed: {},
   methods: {},
   watch: {},
-  created() {}
+  created() {
+  }
 }
 </script>
-
-<style lang="less" scoped>
-.sponsor-node {
-
-}
-</style>
